859d355563d4784002bef64eb101f4afe8ea284e
nigraforce <aleshka2012@gmail.com>
2018-02-01 04:55:03 +0300

    Add files via upload

---
+++ Lab6_2/Lab6_2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+
+namespace Lab6_2
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    class Attr : Attribute
+    {
+        public string description { get; set; }
+        public Attr(string str_description)
+        {
+            description = str_description;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type t = typeof(REFLClass);
+            Console.WriteLine("Конструкторы:");
+            foreach (var x in t.GetConstructors())
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("\nСвойства:");
+            foreach (var x in t.GetProperties())
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("\nМетоды:");
+            foreach (var x in t.GetMethods())
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine("\nАтрибуты свойств:");
+            foreach (var x in t.GetProperties())
+            {
+                var isAttr = x.GetCustomAttributes(typeof(Attr), false);
+                if (isAttr.Length > 0)
+                {
+                    Console.WriteLine(isAttr[0]);
+                }
+            }
+            Console.WriteLine();
+
+            REFLClass class1 = new REFLClass(13, 2);
+            object[] param = new object[] { 4 };
+            t.InvokeMember("PrintDoublePropertyMultiplied", System.Reflection.BindingFlags.InvokeMethod, null, class1, param);
+
+
+
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ Lab6_2/Lab6_2/REFLClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6_2
+{
+    class REFLClass
+    {
+        [Attr("Property, type: double")]
+        public double DoubleProperty1
+        { get; set; }
+        public int IntProperty1
+        { get; set; }
+
+        public REFLClass(double a, int b)
+        {
+            DoubleProperty1 = a;
+            IntProperty1 = b;
+        }
+
+        public REFLClass(double a)
+        {
+            DoubleProperty1 = a;
+            IntProperty1 = Convert.ToInt32(a);
+        }
+
+        public void PrintProperties()
+        {
+            System.Console.WriteLine("DoubleProperty1 = " + DoubleProperty1.ToString() + "\nIntProperty1 = " + IntProperty1.ToString());
+        }
+
+        public void PrintDoublePropertyMultiplied(double mul)
+        {
+            System.Console.WriteLine("DoubleProperty1 * " + mul.ToString() + " = " + (DoubleProperty1 * mul).ToString());
+        }
+
+    }
+}
